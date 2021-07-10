16890d929facc64f1c4e7439bd1afa911a41e167
vovkpavlik <vovk.pavlik@bk.ru>
2021-07-10 11:38:52 +0300

    Обработал ошибки, когда нет книг.

--- main.py
+++ main.py
@@ -3,32 +3,37 @@ import os
 from pathlib import Path
 import requests
 import urllib3
+from urllib3.exceptions import HTTPError
 
 
-def make_directory():
-    directory = "books"
-    import os
+def make_directory(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
     return directory
 
 
-def download_books():
-    directory = make_directory()
-    for book_number in range(10):
-        url = f"https://tululu.org/txt.php?id={book_number}"
-        filename = f"{directory}/book_{book_number}.txt"
-        response = requests.get(url)
-        response.raise_for_status()
-
-        with open(filename, "wb") as file:
-            file.write(response.content)
+def check_for_redirect(redirect):
+    if not redirect:
+        pass
+    else:
+        raise HTTPError
 
 
 def main():
     urllib3.disable_warnings()
-    download_books()
+    
+    folder = make_directory("books")
 
+    for book_number in range(10):
+        response = requests.get(f"https://tululu.org/txt.php?id={book_number}")
+        response.raise_for_status()
+        try:
+            check_for_redirect(response.history)
+            filename = f"{folder}/book_{book_number}.txt"
+            with open(filename, "wb") as file:
+                file.write(response.content)
+        except HTTPError: 
+            print("Такой книги не существует в природе")
 
 
 if __name__ == "__main__":
