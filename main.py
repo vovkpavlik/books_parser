a0ce0d64a569e65689a512d1809eb18e6f8727f9
vovkpavlik <vovk.pavlik@bk.ru>
2021-07-23 19:52:50 +0300

    Добавил параметры для командной строки.

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ import os
 from urllib.parse import urljoin, urlsplit, unquote
 import json
 
-from pprint import pprint
 import requests
 import urllib3
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
 from urllib3.exceptions import HTTPError
 
-from parse_tululu_category import get_books_id, get_books_url
+from parse_tululu_category import get_books_id
 
 
 def check_for_redirect(response):
@@ -18,7 +17,7 @@ def check_for_redirect(response):
         raise HTTPError
 
 
-def download_txt(book_id, download_url, filename, folder="books"):
+def download_txt(book_id, download_url, filename, folder):
     params = {"id":book_id}
     response = requests.get(download_url, params=params)
     response.raise_for_status()
@@ -27,7 +26,7 @@ def download_txt(book_id, download_url, filename, folder="books"):
         file.write(response.text)
 
 
-def download_img(img_url, filename, folder="images"):
+def download_img(img_url, filename, folder):
     response = requests.get(img_url)
     response.raise_for_status()
     check_for_redirect(response)
@@ -73,32 +72,43 @@ def main():
     urllib3.disable_warnings()
 
     parser = argparse.ArgumentParser(description="Качает книжки с сайта тулулу")
-    parser.add_argument('--start_page', default=1, help="стартовая страница", type=int)
-    parser.add_argument('--end_page',default=11, help="конечная страница", type=int)
+    parser.add_argument("--start_page", default=1, help="стартовая страница", type=int)
+    parser.add_argument("--end_page", default=701, help="конечная страница", type=int)
+    parser.add_argument("--texts_folder", default="books_texts", help="путь к текстам книг", type=str)
+    parser.add_argument("--imgs_folder", default="books_images", help="путь к картинкам книг", type=str)
+    parser.add_argument("--json_path", default=os.getcwd(), help="путь к json файлу с информацией о книгах", type=str)
+    parser.add_argument("--skip_imgs", action="store_true")
+    parser.add_argument("--skip_texts", action="store_true")
     args = parser.parse_args()
-
-    os.makedirs("books", exist_ok=True)
-    os.makedirs("images", exist_ok=True)
 
     books_info = []
 
-    books_id = get_books_id(args.start_page, args.end_page+1) 
+    books_id = get_books_id(args.start_page, args.end_page) 
  
     for book_id in books_id:
         download_url = "https://tululu.org/txt.php" 
         book_url = f"https://tululu.org/b{book_id}/"       
+        
         try: 
             book_info = parse_book_page(book_url) 
             books_info.append(parse_book_page(book_url))
-            # img_url = book_info["img_url"]
+            img_url = book_info["img_url"]
             book_title = f"{book_id}.{book_info['title']}"
             filename = sanitize_filename(book_title)
-            # download_txt(book_id, download_url, filename)
-            # download_img(img_url, filename)
+            
+            if not args.skip_imgs:
+                os.makedirs(args.imgs_folder, exist_ok=True)
+                download_img(img_url, filename, args.imgs_folder)
+            if not args.skip_texts:
+                os.makedirs(args.texts_folder, exist_ok=True)
+                download_txt(book_id, download_url, filename, args.texts_folder)
+
         except HTTPError:
             print("Такой книги не существует в природе")
-    
-    with open("books_info.json", "a") as my_file:
+
+    if args.json_path:
+       os.makedirs(args.json_path, exist_ok=True) 
+    with open(os.path.join(args.json_path, "books_info.json"), "a") as my_file:
         json.dump(
             books_info, 
             my_file, 
